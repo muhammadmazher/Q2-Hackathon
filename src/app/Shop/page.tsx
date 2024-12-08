3bc587d3aff2e01712a74119024ebee86db24be2
Muhammad Mazher Khan <muhammadmazher@gmail.com>
2024-12-09 00:40:53 +0500

    3rd commit

--- src/app/Shop/page.tsx
+++ src/app/Shop/page.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
+import { SlidersHorizontal } from 'lucide-react';
+import { LayoutGrid } from 'lucide-react';
 
-import Navbar from "../Component/Navbar";
 
 export default function Shop () {
     return (
         <div>
             <div className="mt-1">
                 <Image src="/shop.png" alt="shop" width={1600} height={300} />
+            </div>
+
+            <div className="w-full flex h-[100px] bg-[#FAF4F4] mt-2 py-8 pl-16">
+                <div className="pt-1">
+                <Link href="/Shop"><SlidersHorizontal /></Link>
+                </div>
+                    <p className="text-xl pl-4">Filter</p>
+
+                <div className="pt-1 pl-4">
+                    <Link href="/Shop"><LayoutGrid /></Link>
+                </div>
+
             </div>
             
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import Navbar from "./Component/Navbar";
+
 import MobileNavbar from "./Component/MobilleNavbar";
 import HeroSection from "./Component/HeroSection";
 import MainPage from "./Component/MainPage";
 import NewArrivals from "./Component/NewArrivals";
 import OurBlogs from "./Component/OurBlogs";
 import FollowUs from "./Component/FollowUs";
-import Footer from "./Component/Footer";
+
 
 export default function Home() {
   return (
